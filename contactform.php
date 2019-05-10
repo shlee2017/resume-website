2d1c5c2ede58a92650d421858ab23309ca3023ad
leesnap <leesnap@umich.edu>
2019-05-10 18:47:48 -0400

    opps

--- contactform.php
+++ contactform.php
@@ -44,7 +44,7 @@
     $message = $_POST['message'];
     $subject = $_POST['subject'];
     $content="From: $name \n Email: $email \n Message: $message";
-    $recipient = "yourmail@yourdomain.com";
+    $recipient = "leesnap@umich.edu";
     $mailheader = "From: $email \r\n";
     mail($recipient, $subject, $content, $mailheader) or die("Error!");
     echo "Email sent!";
